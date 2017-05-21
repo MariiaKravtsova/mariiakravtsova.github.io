d26f456bb75ef02c6689fef481901a460d63580e
MariiaKravtsova <kravtsova.mariia@gmail.com>
2017-05-20 22:47:51 -0400

    Add new about me

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ $("document").ready(function() {
 
 function handleTyping () {
   $(".element").typed({
-    strings: ["a Computer Science student.", "a space nerd.", "a Machine Learning explorer."],
+    strings: ["a software engineer in training.", "a space nerd.", "a bookworm.", "a Machine Learning explorer."],
     typeSpeed: 50,
     starDelay: 200,
     backDelay: 600,
