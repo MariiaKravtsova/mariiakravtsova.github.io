2002cc2c585b4347d1f4ee3b84c1fe3eac6caef2
Mariia Kravtsova <mariia@quikly.com>
2018-12-12 21:54:48 -0500

    Update text

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,7 @@ $("document").ready(function() {
 
 function handleTyping () {
   $(".element").typed({
-    strings: ["a software engineer in training.", "a space nerd.", "a bookworm.", "a Machine Learning explorer."],
+    strings: ["a software engineer.", "a space nerd.", "a curious cat.", "a Machine Learning explorer."],
     typeSpeed: 50,
     starDelay: 200,
     backDelay: 600,
